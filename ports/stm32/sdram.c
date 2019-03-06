079be6d984f9aa7164b1880c223e6a2d9a25bd1f
forester3 <isaka2f@yahoo.co.jp>
2019-03-06 13:33:02 +0900

    clean: move MPU_config to sdram.c

--- ports/stm32/sdram.c
+++ ports/stm32/sdram.c
@@ -237,6 +237,21 @@ static void sdram_init_seq(SDRAM_HandleTypeDef
     /* Disable the MPU */
     HAL_MPU_Disable();
 
+    #if MICROPY_HW_HAS_F7DLCD
+    /* Configure the MPU attributes as Write-Through for Internal SRAM */
+    MPU_InitStruct.Enable 			= MPU_REGION_ENABLE;
+    MPU_InitStruct.BaseAddress 		= 0x20010000;
+    MPU_InitStruct.Size 			= MPU_REGION_SIZE_256KB;
+    MPU_InitStruct.AccessPermission = MPU_REGION_FULL_ACCESS;
+    MPU_InitStruct.IsBufferable 	= MPU_ACCESS_NOT_BUFFERABLE;
+    MPU_InitStruct.IsCacheable 		= MPU_ACCESS_CACHEABLE;
+    MPU_InitStruct.IsShareable 		= MPU_ACCESS_NOT_SHAREABLE;
+    MPU_InitStruct.Number 			= MPU_REGION_NUMBER0;
+    MPU_InitStruct.TypeExtField 	= MPU_TEX_LEVEL0;
+    MPU_InitStruct.SubRegionDisable = 0x00;
+    MPU_InitStruct.DisableExec 		= MPU_INSTRUCTION_ACCESS_ENABLE;
+    HAL_MPU_ConfigRegion(&MPU_InitStruct);
+    #endif
     /* Configure the MPU attributes as Write-Through for External SDRAM */
     MPU_InitStruct.Enable = MPU_REGION_ENABLE;
     MPU_InitStruct.BaseAddress = SDRAM_START_ADDRESS;
@@ -245,12 +260,11 @@ static void sdram_init_seq(SDRAM_HandleTypeDef
     MPU_InitStruct.IsBufferable = MPU_ACCESS_NOT_BUFFERABLE;
     MPU_InitStruct.IsCacheable = MPU_ACCESS_CACHEABLE;
     MPU_InitStruct.IsShareable = MPU_ACCESS_NOT_SHAREABLE;
-    MPU_InitStruct.Number = MPU_REGION_NUMBER0;
+    MPU_InitStruct.Number = MPU_REGION_NUMBER1;
     MPU_InitStruct.TypeExtField = MPU_TEX_LEVEL0;
     MPU_InitStruct.SubRegionDisable = 0x00;
     MPU_InitStruct.DisableExec = MPU_INSTRUCTION_ACCESS_ENABLE;
     HAL_MPU_ConfigRegion(&MPU_InitStruct);
-
     /* Enable the MPU */
     HAL_MPU_Enable(MPU_PRIVILEGED_DEFAULT);
     #endif
